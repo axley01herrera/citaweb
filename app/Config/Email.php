d3bd1b848ac6465a6d3ca0e67bb5719ed5006d85
Axley Herrera <axley01herrera@gmail.com>
2023-09-22 21:08:20 +0100

    updates

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -54,14 +54,14 @@ class Email extends BaseConfig
      *
      * @var string
      */
-    public $SMTPUser = 'info@grupoahvsolucionesinformaticas.es';
+    public $SMTPUser = '';
 
     /**
      * SMTP Password
      *
      * @var string
      */
-    public $SMTPPass = '@NICOLE6062017hOSTINGER';
+    public $SMTPPass = '';
 
     /**
      * SMTP Port
