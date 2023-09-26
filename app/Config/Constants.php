b758f67d248e073aeee8edaa8459ecfd13c8b3fa
Axley Herrera <axley01herrera@gmail.com>
2023-09-26 12:05:09 +0100

    Constants

--- app/Config/Constants.php
+++ app/Config/Constants.php
@@ -78,8 +78,8 @@ defined('EXIT_DATABASE')       || define('EXIT_DATABASE', 8); // database error
 defined('EXIT__AUTO_MIN')      || define('EXIT__AUTO_MIN', 9); // lowest automatically-assigned error code
 defined('EXIT__AUTO_MAX')      || define('EXIT__AUTO_MAX', 125); // highest automatically-assigned error code
 
-define("ENVIRONMENT","production");
-// define("ENVIRONMENT","development");
+//define("ENVIRONMENT","production");
+define("ENVIRONMENT","development");
 define("EMAIL_PROTOCOL","smtp");
 define("EMAIL_SMTP_HOST","ssl://smtp.hostinger.com");
 define("EMAIL_SMTP_USER","dev@axleyherrera.com");
